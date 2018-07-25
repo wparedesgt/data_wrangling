4e5fcc231e6b4f00af4a6b2316a518527e51a880
wparedesgt <wparedesgt@gmail.com>
2018-07-25 13:56:05 -0600

    Leer xlsx

---
+++ Importing_Data_Using_R-base.R
@@ -0,0 +1,1 @@
+##Importando usando funciones en R
---
+++ readr_and_readxl.R
@@ -0,0 +1,28 @@
+### Funciones para leer excel 
+
+## read_excel lee xls y xlsx
+## read_xls unicamente lee xls 
+## read_xlsx unicamente lee xlsx
+## excel_sheets nombra las paginas de una hoja de excel y este nombre se puede
+## pasar a las tres funciones anteriores.
+## read_lines funcion para leer lineas
+
+library(tidyverse)
+library(readxl)
+
+
+read_lines("data/murders.csv", n_max = 3)
+
+dat <- read.csv("data/murders.csv")
+
+head(dat)
+tail(dat)
+str(dat)
+
+getwd()
+
+#times2016 <- read_excel("data/times.xlsx", sheet = 2)
+##  error times2016 <- read_xlsx("data/times.xlsx", sheet = "2")
+#times2016 <- read_xlsx("data/times.xlsx", sheet = 2)
+
+times2016 <- read_excel("data/times.xlsx", sheet = "2016")
